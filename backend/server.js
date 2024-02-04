3dded63674e8b39fcf9dbe9061135a8d2f60f6b5
Jvzqz13 <j.vzqz13@gmail.com>
2024-02-04 16:34:39 -0500

    server running

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import './loadEnv.js'
+import express from 'express'; 
+import morgan from 'morgan';
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the API')
+})
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT: ${PORT}`);
+})
+

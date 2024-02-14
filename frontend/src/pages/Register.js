ae96133c737442f67767ec692a9df87f84f6673f
Jvzqz13 <j.vzqz13@gmail.com>
2024-02-14 16:41:47 -0500

    deleted a coment

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -10,8 +10,6 @@ function Register () {
     const handleRegister = async (formData) =>{
         try {
             const res = await axios.post('https://comedy-backend.onrender.com/api/users/register', 
-            // const res = await axios.post('http://localhost:4000/api/users/register', //<==testing
-            
             {
                 email: formData.email,
                 password: formData.password
